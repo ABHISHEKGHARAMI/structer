2a4156bc557827085c489c4fa0e7e13b582d8912
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-01-06 11:45:52 +0530

    Max Subarray Problem

--- array/max_subarray_sum_ele1.py
+++ array/max_subarray_sum_ele1.py
@@ -14,6 +14,31 @@ def kadane(arr):
             max_so_far = max_ending_here
     return max_so_far
 
+
+# program for the return the elemnet of the array form the original array
+def element_kadane(arr):
+    if not arr:
+        return 0,[]
+    else:
+        max_sum = current_sum = arr[0]
+        start = end = 0
+        current_state = 0
+        
+        for i in range(1,len(arr)):
+            if arr[i] > current_sum+arr[i]:
+                current_sum = arr[i]
+                current_start = i
+            else:
+                current_sum+=arr[i]
+            
+            if current_sum > max_sum:
+                max_sum = current_sum
+                start = current_start
+                end = i
+        
+        return max_sum , arr[start:end+1]
+    
 a = [-2, -3, 4, -1, -2, 1, 5, -3]
 print(kadane(a))
-            
+c , b = element_kadane(a)
+print(f"The max sum is : {c} and the array giving the sum is : {b}")
