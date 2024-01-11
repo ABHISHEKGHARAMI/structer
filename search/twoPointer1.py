35f24a7ab0f1adf8ff6f8c5b784e0946bd434fb0
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-01-11 21:02:44 +0530

    With Two Pointer technique

--- search/twoPointer1.py
+++ search/twoPointer1.py
@@ -14,11 +14,26 @@ def naive(arr,x):
     else:
         return False
     
-    
+# here goes the two pointer  technique
+def two_pointer(arr,start,end,val):
+    if start <= end:
+        if arr[start] + arr[end] == val:
+            return True
+        elif arr[start] + arr[end] > val:
+            return two_pointer(arr,start,end - 1,val)
+        else:
+            return two_pointer(arr,start+1,end, val)
+    else:
+        return False
 arr = [ 2,3,8 ,11]
 x = 14
 
-if naive(arr,x) == 1:
+"""if naive(arr,x) == 1:
     print("Pair.")
 else:
-    print("Dont have pair.")
+    print("Dont have pair.")"""
+    
+if two_pointer(arr,0,len(arr)-1,x) == True:
+    print("pair exsist.")
+else:
+    print("dont have pair.")
