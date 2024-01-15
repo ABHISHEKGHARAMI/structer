6afae1e42187a713c6ac0cff2815e0e62b608204
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-01-15 16:42:03 +0530

    Trying to solve the invertion count with merge sort approach

--- sorting/invertionCount1.py
+++ sorting/invertionCount1.py
@@ -28,7 +28,46 @@ def naive_invertionCount(arr):
                 icount+=1
     return icount
 
+# invertion using the merge sort
+def mergeSort(arr):
+    n = len(arr)
+    temp = [0]*n
+    return _mergeSort(arr,temp,0,n-1)
+    
 
+def _mergeSort(arr,temp,left,right):
+    icount = 0
+    if left < right:
+        mid = (left + right) //2
+        icount += _mergeSort(arr,temp,left,mid-1)
+        icount += _mergeSort(arr,temp,mid+1,right)
+        icount += merge(arr,temp,left,mid,right)
+    return icount
+
+def merge(arr,temp,left,mid,right):
+    i = left
+    j = mid + 1
+    k = left
+    icount = 0
+    while i <= mid and j <= right:
+        if arr[i] < arr[j]:
+            temp[k] = arr[i]
+            i+=1
+            k+=1
+        else:
+            temp[k] = arr[j]
+            icount += mid -i
+            k+=1
+            j+=1
+    while i<= mid:
+        temp[k] = arr[i]
+        i+=1
+        k+=1
+    while j <= right:
+        temp[k] = arr[j]
+        j+=1
+        k+=1
+    return icount
 arr = [8, 4, 2, 1]
 
-print(f"The Invertion count of the array is : {naive_invertionCount(arr)}")
+print(f"The Invertion count of the array is : {mergeSort(arr)}")
