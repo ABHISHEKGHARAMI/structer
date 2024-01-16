3d1aae3e0396a3e15468be0fe9bb39eff908eb3c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-01-16 16:49:09 +0530

    Updating the QuickSort Algorithm

--- sorting/naiveQuickSort1.py
+++ sorting/naiveQuickSort1.py
@@ -1,4 +1,39 @@
 # Naive Quick Sort Algorithm 
+
+
+'''
+The key process in quickSort is a partition(). 
+The target of partitions is to place the pivot (any element can be chosen to be a pivot) 
+at its correct position in the sorted array and put all smaller elements to the left of the pivot,
+and all greater elements to the right of the pivot.
+
+Partition is done recursively on each side of the pivot after the pivot 
+is placed in its correct position and this finally sorts the array.
+
+
+
+sudo algorithm :
+
+Naive partition(arr[],l,r)
+1. Make a Temporary array temp[r-l+1] length
+2. Choose last element as a pivot element
+3. Run two loops:
+    -> Store all the elements in the temp array that are less than pivot element
+    -> Store the pivot element 
+    -> Store all the elements in the temp array that are greater than pivot element.
+4.Update all the elements of arr[] with the temp[] array    
+
+
+QuickSort(arr[], l,  r)
+If r > l
+     1. Find the partition point of the array  
+              m = Naivepartition(a,l,r) 
+     2. Call Quicksort for less than partition point   
+             Call Quicksort(arr, l, m-1)
+     3. Call Quicksort for greater than the partition point 
+             Call Quicksort(arr, m+1, r)
+
+'''
 def QuickSort(arr,low,high):
     if low < high:
         pi = partition(arr,low,high)
