085a1cb897125125a99856f87eaeb274450ec3bc
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-01-20 15:31:58 +0530

    After the right segregation for the array .

--- sorting/segregation1.py
+++ sorting/segregation1.py
@@ -17,13 +17,33 @@ def segregation(arr):
     zero = 0
     one = n - 1
     while zero < one:
-        if arr[zero] < arr[one]:
+        if arr[zero] == 1:
             arr[zero] , arr[one] = arr[one] , arr[zero]
             one = one - 1
         else:
             zero = zero + 1
-            
+
+# what if the 1 should be in the left side and zero can be the right side.
+
+
+def segregationRight(arr):
+    n = len(arr)
+    zero = 0
+    one = n - 1
+    while zero < one:
+        if arr[zero] == 0:
+            arr[zero] , arr[one] = arr[one] , arr[zero]
+            one = one -1
+        else:
+            zero = zero + 1
+
 arr = [1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1]
 segregation(arr)
+print(arr)
+
+# for the segregation right for the array
+segregationRight(arr)
+print("After the right segregation for the array :")
+segregationRight(arr)
 print(arr)
     
