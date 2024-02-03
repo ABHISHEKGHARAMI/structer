206e6513d6a5a3e0fc063bff0e8b4d12bfff9271
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-03 10:47:27 +0530

    Done roation without extra space .

---
+++ matrix/matrixRotationSpace1.py
@@ -0,0 +1,17 @@
+# matrix rotation 90 anti clock wise without using the extra space
+
+def matrixRotation90(mat):
+    n = len(mat)
+    for i in range(n//2):
+        j = i
+        while j < n - i - 1:
+            temp = mat[i][j]
+            mat[i][j] = mat[j][n - i -1]
+            mat[j][n - i - 1] = mat[n - i - 1][n - j - 1]
+            mat[n - i - 1][n - j - 1] = mat[n - i - 1][j]
+            mat[n - i - 1][j] = temp
+            j+=1
+    return mat
+    
+mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+print(f"After the matrix rotation the matrix will be : {matrixRotation90(mat)}")
