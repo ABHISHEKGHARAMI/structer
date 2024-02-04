11e6617bfe8a25de9ce7b74f374bf422b12b147f
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-04 13:29:30 +0530

    Adding the repr method .

---
+++ string/basics/usingRepr1.py
@@ -0,0 +1,9 @@
+# printing the escape charecter in python
+ch = "I\nLove\tGeeksforgeeks"
+# first we will print with out the repr
+print(ch)
+
+
+# after using the repr method the string will be print as well directed.
+
+print(repr(ch))
