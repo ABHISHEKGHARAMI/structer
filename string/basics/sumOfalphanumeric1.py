340344d49e9169c1861f3dcb91c88c0c83909cdf
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-07 23:15:21 +0530

    Sum of Alpha Numeric Charecter  in repo .

--- string/basics/sumOfalphanumeric1.py
+++ string/basics/sumOfalphanumeric1.py
@@ -13,4 +13,17 @@ string which is added to get the sum as
 
 '''
 def sumAlphaNumeric(Str1):
-    pass
+    total_sum = 0
+    current_sum = 0
+    for char in Str1:
+        if char.isdigit():
+            current_sum = current_sum * 10 + int(char)
+        else:
+            total_sum += current_sum
+            current_sum = 0
+    total_sum += current_sum
+    return total_sum
+
+
+str1 = "1abc23"
+print(f"The total sum of the alpha numeric charecter : {sumAlphaNumeric(str1)}")
