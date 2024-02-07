f217d6737f5f485d7dcd5f246d6dcd3a7a8ac48b
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-07 14:49:51 +0530

    Sum of Alphanumeric Charecter added in repo .

---
+++ string/basics/sumOfalphanumeric1.py
@@ -0,0 +1,16 @@
+'''
+Given a string str containing alphanumeric characters.
+The task is to calculate the sum of all the numbers present in the string.
+
+Example 1:
+
+Input:
+str = 1abc23
+Output: 24
+Explanation: 1 and 23 are numbers in the
+string which is added to get the sum as
+24.
+
+'''
+def sumAlphaNumeric(Str1):
+    pass
