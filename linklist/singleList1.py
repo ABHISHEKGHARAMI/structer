48c3888c269b3c7691b768599fd9b2f20c311589
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-17 23:38:39 +0530

    Updating work done .

--- linklist/singleList1.py
+++ linklist/singleList1.py
@@ -2,15 +2,16 @@
 Single list implementation for linked list using python.
 
 there are several operation need to follow
- i. insert at the front
- ii. insert at the end
+ i. insert at the front d
+ ii. insert at the end d
  iii. insert after a certain node
  iv. insert before a certain node
  v. delete from the front
  vi . delete from the end
  vii. delete after a certain node
  viii. delete before a certain node
- ix. print the list.
+ ix. print the list. d
+ x . search from the list d
  and more ..
  one by one we will implement this.
 '''
