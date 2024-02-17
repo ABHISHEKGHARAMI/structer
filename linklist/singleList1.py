10c5b97cc455e17362dad08a471eeb8b9ba63d8a
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-17 21:10:52 +0530

    Completing the LinkList for the Data structures .

--- linklist/singleList1.py
+++ linklist/singleList1.py
@@ -1,10 +1,18 @@
 '''
-Single Link List implement in python
- different types of operation will learn in later like
- i . insert at the front,end,after or front of the node
- ii. delete node from the front,end , after or front of the node.
- iii. display all the nodes.
- 
+Single list implementation for linked list using python.
+
+there are several operation need to follow
+ i. insert at the front
+ ii. insert at the end
+ iii. insert after a certain node
+ iv. insert before a certain node
+ v. delete from the front
+ vi . delete from the end
+ vii. delete after a certain node
+ viii. delete before a certain node
+ ix. print the list.
+ and more ..
+ one by one we will implement this.
 '''
 
 class Node:
@@ -13,40 +21,8 @@ class Node:
         self.next = None
         
         
-# class for the linked list
-class LinkedList:
+        
+# class for link list
+class LinkList:
     def __init__(self):
         self.head = None
-        
-        
-    # insert at the front
-    def insertFront(self,data):
-        if self.head is None:
-            new_node = Node(data)
-            new_node.next = self.head
-            self.head = new_node
-            
-            
-            
-    # insert at the end of the linked list
-    def insertEnd(self,data):
-        if self.head is None:
-            self.insertFront(data)
-        else:
-            temp = self.head
-            while temp.next:
-                temp = temp.next
-            new_node = Node(data)
-            temp.next = new_node
-            new_node.next = None
-            
-    # display the linked list
-    def display(self):
-        if self.head is None:
-            print("the linked list is empty.")
-        else:
-            temp = self.head
-            while temp.next :
-                print(f"=>{temp.data}")
-                temp = temp.next
-            
