69b1be34c34bca5c6c7df048fc634eadc3a11939
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-26 16:38:23 +0530

    Corrected the problem.

--- linklist/sub2List1.py
+++ linklist/sub2List1.py
@@ -18,44 +18,47 @@ lists are 786 and  789, so 789 - 786 is 3,
 as the smaller value is subtracted from 
 the larger one. '''
 
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
-        
+
+# Linked list implementation
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
-        
-        
-    # insert the list
-    def insertList(self,data):
+
+    # Print the linked list
+    def printingList(self):
+        print("\nPrinting the linked list...\n")
+        if self.head is None:
+            print("\nList is empty.\n")
+        else:
+            temp = self.head
+            while temp:
+                print(f"{temp.data}=>", end=" ")
+                temp = temp.next
+
+    # Insert data into the linked list
+    def insertList(self, data):
         new_node = Node(data)
         if self.head is None:
             new_node.next = self.head
             self.head = new_node
         else:
+            # Append the data
             temp = self.head
             while temp.next:
                 temp = temp.next
             temp.next = new_node
-            
-            
-    # print the list
-    def printList(self):
-        if self.head is None:
-            print("\nthe list is empty.\n")
-        else:
-            temp = self.head
-            while temp:
-                print(temp.data,end=" ")
-                temp = temp.next
-                
-                
-    # reverse the list
+
+    # Reverse the list
     def reverseList(self):
         if self.head is None:
-            print("\nthe list is empty.\n")
+            print("\nThe list is empty.\n")
         else:
             prev = None
             temp = self.head
@@ -65,13 +68,10 @@ class LinkedList:
                 prev = temp
                 temp = next
             self.head = prev
-            
-            
-                
-                
-    # substraction of 2 lists
-    def sub2List(self,list1,list2):
-        borrow = 0 
+
+    # Subtract two linked lists
+    def subtractLinkedLists(self, list1, list2):
+        borrow = 0
         result = LinkedList()
         ptr1 = list1.head
         ptr2 = list2.head
@@ -89,31 +89,31 @@ class LinkedList:
                 ptr1 = ptr1.next
             if ptr2:
                 ptr2 = ptr2.next
-            
         return result
-    
-    
-# testing the substraction result
+
+
+# Testing the linked list
 list1 = LinkedList()
 list1.insertList(1)
-list1.insertList(2)
 list1.insertList(0)
-
+list1.insertList(0)
 
 list2 = LinkedList()
-list1.insertList(1)
-list1.insertList(0)
-list1.insertList(0)
+list2.insertList(9)
+list2.insertList(9)
 
-
-#reverse the list
+# Reverse the lists
 list1.reverseList()
 list2.reverseList()
 
-result = LinkedList().sub2List(list1,list2)
+# Subtract the lists
+result = LinkedList().subtractLinkedLists(list1, list2)
 
+# Reverse the result list back to original order
 result.reverseList()
 
-result.printList()
+# Print the result
+result.printingList()
+
             
             
