b1ddab84483ea981254f865895375c38c2dbebb4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-26 10:17:10 +0530

    Add 2 number two linked list .

---
+++ linklist/add2Numberlist1.py
@@ -0,0 +1,22 @@
+# add two number of the linked list and print the new linked list
+'''
+Given two numbers represented by two lists, write a function that returns the sum 
+in the form of a linked list.
+
+Example:
+
+Input: 
+List1: 5->6->3 // represents number 563 
+List2: 8->4->2 // represents number 842 
+Output: 
+Resultant list: 1->4->0->5 // represents number 1405 
+Explanation: 563 + 842 = 1405
+
+Input: 
+List1: 7->5->9->4->6 // represents number 75946
+List2: 8->4 // represents number 84
+Output: 
+Resultant list: 7->6->0->3->0// represents number 76030
+Explanation: 75946+84=76030
+
+'''
