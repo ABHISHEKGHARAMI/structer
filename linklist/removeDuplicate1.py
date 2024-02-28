a32fa9041d8546a02deab892b09464d55a8a0cb2
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-28 23:42:24 +0530

    Add new question remove duplicate from the  linked list .

---
+++ linklist/removeDuplicate1.py
@@ -0,0 +1,47 @@
+'''
+ Remove the duplicate from the linked list 
+ 
+ Write a function that takes a list sorted in non-decreasing order and deletes any 
+ duplicate nodes from the list. The list should only be traversed once. 
+For example if the linked list is 11->11->11->21->43->43->60 then 
+removeDuplicates() should convert the list to 11->21->43->60. 
+
+'''
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# linked list
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # insert the new node or append in the linked list
+    def insertList(self, data):
+        print("\ninserting the new data in the linked list ...\n")
+        new_node = Node(data)
+        if self.head is None:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = new_node
+
+    # printing the linked list
+
+    def printingList(self):
+        if self.head is None:
+            print("\nthe list is empty.....\n")
+        else:
+            print("\nprinting the list ...\n")
+            temp = self.head
+            while temp.next:
+                print(f"{temp.data}=>", end=" ")
+                temp = temp.next
