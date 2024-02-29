7b557ad26dbf190dcf218333dac99160c0216703
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-02-29 11:30:54 +0530

    Adding problem to the delivery queue .

---
+++ linklist/linkedListIntersection1.py
@@ -0,0 +1,9 @@
+'''
+
+There are two singly linked lists in a system. By some programming error, 
+
+the end node of one of the linked lists got linked to the second list,
+
+forming an inverted Y-shaped list. Write a program to get the point where two linked lists merge. 
+
+'''
