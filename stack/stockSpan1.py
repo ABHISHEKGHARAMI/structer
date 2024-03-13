18328cfc65af5214c3f43ec2e848bed852c437f2
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-03-13 21:25:30 +0530

    Adding Stock Span problem .

---
+++ stack/stockSpan1.py
@@ -0,0 +1,86 @@
+# problem statement
+'''
+
+'''
+# setting up the logger file
+import sys
+sys.path.append("D:/geeks1.0/structer")
+
+from settings import setup_logging
+import logging
+
+setup_logging()
+
+# class for stack
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    # push operation for the stack
+    def pushStack(self, data):
+        if data is None:
+            print(f"\nData is empty.")
+            logging.error("Data is empty.")
+        else:
+            print(f"Inserting the {data}")
+            logging.info(f"Inserting the {data}")
+            self.stack.append(data)
+
+    # isEmpty for the stack
+
+    def isEmpty(self):
+        if len(self.stack) == 0:
+            return 1
+        else:
+            return 0
+
+    # pop for the stack
+    def popStack(self):
+        if self.isEmpty() == 1:
+            print("\nThe stack is empty.")
+            logging.error("Stack is empty.")
+        else:
+            element = self.stack.pop()
+            print(f"{element} has been pop from the stack.")
+            logging.info(f"{element} has been deleted.")
+
+    # size of the stack
+
+    def sizeStack(self):
+        if self.isEmpty() == 1:
+            return 0
+        else:
+            return len(self.stack)
+
+    # print the stack
+    def printStack(self):
+        if self.isEmpty():
+            print("\nThe stack is empty.")
+        else:
+            for i in range(len(self.stack)):
+                print(f"\n{self.stack[i]}", end=" ")
+            logging.info(self.stack)
+
+    # top of the element of the stack
+
+    def topStack(self):
+        if self.isEmpty() == 1:
+            return 0
+        else:
+            return self.stack[-1]
+
+    # function for value containing
+    def __contain__(self, data):
+        if data is None:
+            return 0
+        else:
+            flag = 0
+            for i in range(len(self.stack)):
+                if self.stack[i] == data:
+                    flag = 1
+                    break
+            if flag == 1:
+                return 1
+            return 0
