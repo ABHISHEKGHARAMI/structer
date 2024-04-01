9787cbe1d566703f57c6e02525951aa4a75db885
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-01 14:31:17 +0530

    Added the networkx module for the tree repo .

--- Tree/NormalTree/noarmalTree1.py
+++ Tree/NormalTree/noarmalTree1.py
@@ -10,3 +10,94 @@ sys.path.append("D:/geeks1.0/structer")
 
 # here goes the tree
 # importing the modules for that
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Treenodes:
+    def __init__(self,value):
+        self.value = value
+        self.children = []
+        
+        
+class Tree:
+    def __init__(self):
+        self.root = None
+    
+    # for finding the node
+    def _find_node(self,node,value):
+        if node.data == value:
+            return node
+        for child in node.children:
+            found = self._find_node(child,value)
+            if found:
+                return found
+        return None
+    
+    # find the parent and children
+    def _find_parent_child(self,node,value):
+        for child in node.children:
+            if child.data == value:
+                return node,child
+            parent, node_to_delete = self._find_parent_child(child,value)
+            if node_to_delete:
+                return parent, node_to_delete
+            return None,None
+    
+    # for inserting the data in the tree
+    def insertData(self,parent_data,data):
+        if not self.root:
+            self.root = Treenodes(data)
+        parent_node = self._find_node(self.root,parent_data)
+        if parent_node:
+            parent_node.append(data)
+            return True
+        return None
+    
+    
+    # delete the node for the tree
+    def deleteNode(self,data):
+        if not self.root:
+            return False
+        if self.root.data == data:
+            self.root = None
+            return True
+        parent_node , node_to_delete = self._find_parent_child(self.root,value)
+        if parent_node and node_to_delete:
+            parent_node.children.remove(node_to_delete)
+            return True
+        return False
+    
+    # function for networks
+    def to_networkx(self):
+        G = nx.Graph()
+        self.add_Edges(self.root,G)
+        return G
+    
+    
+    # def add edges
+    def add_Edges(self, node , G):
+        for child in node.children:
+            G.add_edge(node.data, child.data)
+            self.add_Edges(child,G)
+        
+        
+    # Executing the main function
+    
+tree = Tree()
+tree.insertData(None,1)
+tree.insertData(1,2)
+tree.insertData(1,3)
+tree.insertData(2,4)
+tree.insertData(2,5)
+
+G = tree.to_networkx()
+
+# time to plot the tree
+plt.figure(figsize = (8,6))
+pos = nx.spring_layout(G)  # Positions for all nodes
+nx.draw(G, pos, with_labels=True, node_size=5000, node_color="skyblue",
+        font_size=20, font_weight="bold", arrowsize=20)
+plt.title("Tree Visualization")
+plt.show()
+        
