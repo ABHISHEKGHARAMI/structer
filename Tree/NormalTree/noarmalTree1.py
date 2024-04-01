43b19f0d88165acd1ce6125e610742c9f00ec37d
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-01 11:25:07 +0530

    Adding more documentation for the repo module used .

---
+++ Tree/NormalTree/noarmalTree1.py
@@ -0,0 +1,12 @@
+# building the basic tree data structure (not binary tree) for normal tree
+# and generate the real picture of the tree
+
+# *** setting up the logger file
+import logging
+from settings import setup_logging
+import sys
+sys.path.append("D:/geeks1.0/structer")
+
+
+# here goes the tree
+# importing the modules for that
