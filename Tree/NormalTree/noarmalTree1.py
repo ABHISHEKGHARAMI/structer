e7e67c6068a3c77e1a7b11fb3bee2964d7218046
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-01 15:55:08 +0530

    mod .

--- Tree/NormalTree/noarmalTree1.py
+++ Tree/NormalTree/noarmalTree1.py
@@ -2,8 +2,8 @@
 # and generate the real picture of the tree
 
 # *** setting up the logger file
-import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib.pyplot as plt
 import sys
 sys.path.append("D:/geeks1.0/structer")
 import logging
@@ -25,7 +25,7 @@ class Tree:
     
     # for finding the node
     def _find_node(self,node,value):
-        if node.data == value:
+        if node.value == value:
             return node
         for child in node.children:
             found = self._find_node(child,value)
@@ -36,7 +36,7 @@ class Tree:
     # find the parent and children
     def _find_parent_child(self,node,value):
         for child in node.children:
-            if child.data == value:
+            if child.value == value:
                 return node,child
             parent, node_to_delete = self._find_parent_child(child,value)
             if node_to_delete:
@@ -49,7 +49,7 @@ class Tree:
             self.root = Treenodes(data)
         parent_node = self._find_node(self.root,parent_data)
         if parent_node:
-            parent_node.append(data)
+            parent_node.children.append(data)
             return True
         return None
     
