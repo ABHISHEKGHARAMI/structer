9322ba9b56442afde5121d67f66a1c0bbcdf7034
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-03 12:04:31 +0530

    Adding the binary tree with the networkx .

---
+++ Tree/BinaryTree/bianryTree1.py
@@ -0,0 +1,113 @@
+# implementation of the binary tree
+#importing the module
+import matplotlib.pyplot as plt
+import networkx as nx
+import os
+# setting up the logger
+import sys
+sys.path.append("D:/geeks1.0/structer")
+from settings import setup_logging
+import logging
+
+setup_logging()
+
+# building the tree node
+class TreeNode:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+# building the tree class
+class Tree:
+    def __init__(self):
+        self.root = None
+        
+    # inserting the value for the tree
+    def insertNode(self,data):
+        try:
+            if not self.root:
+                self.root = TreeNode(data)
+            else:
+                self.insertNode_Recursive(self.root,data)
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            
+            
+    # recursive function for the value for the data
+    def insertNode_Recursive(self,node,value):
+        try:
+            if node.value > value :
+                if node.left :
+                    self.insertNode_Recursive(node.left,value)
+                else:
+                    node.left = TreeNode(value)
+            elif node.value < value:
+                if node.right :
+                    self.insertNode_Recursive(node.right,value)
+                else:
+                    node.right = TreeNode(value)
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            
+            
+    
+    # adding the networkx for the graph plotting
+    def to_networkx(self):
+        try:
+            G = nx.Graph()
+            if self.root:
+                self._add_edges(self.root,G)
+            return G
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            
+            
+    # adding the edges of the tree nodes
+    def _add_edges(self,node,G):
+        if node.left:
+            G.add_edge(node.value,node.left.value)
+            logging.info(f"Adding edge between {node.value} and {node.left}")
+            self._add_edges(node.left,G)
+        if node.right:
+            G.add_edge(node.value,node.right.value)
+            self._add_edges(node.right,G)
+            logging.info(f"Adding edge between {node.value} and {node.right}")
+            
+    
+    # visualize and save the image
+    def visualize_saveImage(self,file_path):
+        G = self.to_networkx()
+        plt.figure(figsize=(8,6))
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_size=5000, node_color="skyblue",
+                font_size=20, font_weight="bold", arrowsize=20)
+        plt.title("Binary Tree Visualization")
+
+        # Save the plot to the specified directory
+        plt.savefig(file_path)
+        plt.close()  # Close the plot to free resources
+
+#executing the main function .
+tree = Tree()
+tree.insertNode(5)
+tree.insertNode(3)
+tree.insertNode(7)
+tree.insertNode(2)
+tree.insertNode(4)
+tree.insertNode(6)
+tree.insertNode(8)
+
+current_directory = os.getcwd()
+relative_directory = 'tree_image'
+
+full_dir = os.path.join(current_directory,relative_directory)
+
+if not os.path.exists(full_dir):
+    os.makedirs(full_dir)
+file_path = os.path.join(full_dir,'binary_tree1.png')
+
+tree.visualize_saveImage(file_path=file_path)
