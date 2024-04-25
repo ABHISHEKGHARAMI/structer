4ee4e86ce417cb1ddedfc78fabc9f540fbcd1d42
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-25 19:51:51 +0530

    Adding the right view of the tree .

--- Tree/BinaryTree/binary_practise_tree3.py
+++ Tree/BinaryTree/binary_practise_tree3.py
@@ -298,10 +298,10 @@ class Tree:
                 if i == 0:
                     print(f"->{temp.data}",end=" ")
                     logging.info(f"->{temp.data}")
-                    if temp.left:
-                        q1.push(temp.left)
                     if temp.right:
                         q1.push(temp.right)
+                    if temp.left:
+                        q1.push(temp.left)
                 
             
                     
