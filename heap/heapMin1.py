05d999bead6d23b591371eb8fabbfcac6b054960
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-04 11:52:59 +0530

    Adding the extract from the heap in the repo .

--- heap/heapMin1.py
+++ heap/heapMin1.py
@@ -29,6 +29,32 @@ class Heap:
         self.heap.append(data)
         self.heapify_up(len(self.heap)-1)
         
+        
+    # function for the delete heap element
+    def extractHeap(self):
+        if len(self.heap) == 0:
+            return
+        min_value = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        del self.heap[-1]
+        self.heapify_down(0)
+        return min_value
+    
+    
+    # heapify down  function for the delete
+    def heapify_down(self,i):
+        min_index = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left <len(self.heap) and self.heap[min_index] > self.heap[left]:
+            min_index = left
+        if right < len(self.heap) and self.heap[min_index] > self.heap[right]:
+            min_index = right
+            
+        if i != min_index:
+            self.heap[i], self.heap[min_index] = self.heap[min_index] , self.heap[i]
+            self.heapify_down(min_index)
+        
     def print_heap(self):
         if len(self.heap) == 0:
             return
@@ -40,7 +66,7 @@ class Heap:
                 
 h1 = Heap()
 while True:
-    print("\n 0: Exit. \t1: Insert. \t2: Print Heap.")
+    print("\n 0: Exit. \t1: Insert. \t2: Print Heap. \3: Extract heap.")
     choice = int(input("enter choice :"))
     if choice == 0:
         exit(0)
@@ -52,5 +78,10 @@ while True:
         print("\nPrinting the heap.")
         logging.info("\nPrinting the heap.")
         h1.print_heap()
+        
+    elif choice == 3:
+        min_value = h1.extractHeap()
+        print(f"the min value extracted from the heap is : {min_value}")
+        logging.info(f"the min value extracted from the heap is : {min_value}")
         
         
