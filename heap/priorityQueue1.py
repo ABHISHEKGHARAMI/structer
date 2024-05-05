270be65ee3909b94356e9d9036d3c4701cc677e1
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-05 12:46:12 +0530

    mod .

--- heap/priorityQueue1.py
+++ heap/priorityQueue1.py
@@ -10,80 +10,69 @@ setup_logging()
 class PriorityQueue:
     def __init__(self):
         self.heap = []
-        
-    def parent(self,i):
-        return (i - 1)//2
-    
-    def left(self,i):
+
+    def parent(self, i):
+        return (i - 1) // 2
+
+    def left_child(self, i):
         return 2 * i + 1
-    
-    def right(self,i):
+
+    def right_child(self, i):
         return 2 * i + 2
-    
-    def insertPQ(self,data):
-        print(f"inserting the {data} in the priority queue.")
-        logging.info(f"inserting the {data} in the priority queue.")
-        self.heap.append(data)
-        self.heapify_up(len(self.heap)-1)
-    
-    def heapify_up(self,i):
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.heapify_up(len(self.heap) - 1)
+
+    def heapify_up(self, i):
         while i > 0 and self.heap[self.parent(i)] > self.heap[i]:
-            self.heap[self.parent(i)],self.heap[i] = self.heap[i],self.heap[self.parent(i)]
+            self.heap[self.parent(
+                i)], self.heap[i] = self.heap[i], self.heap[self.parent(i)]
             i = self.parent(i)
-            
-    def extractMin(self):
-        min_value = self.heap[0]
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            return None
+        min_val = self.heap[0]
         self.heap[0] = self.heap[-1]
         del self.heap[-1]
-        self.heapify_down(len(self.heap))
-        return min_value
-    
-    def heapify_down(self,i):
+        self.heapify_down(0)
+        return min_val
+
+    def heapify_down(self, i):
         min_index = i
-        left = self.left(i)
-        right = self.right(i)
-        if left < len(self.heap) and self.heap[left] > self.heap[min_index]:
+        left = self.left_child(i)
+        right = self.right_child(i)
+        if left < len(self.heap) and self.heap[left] < self.heap[min_index]:
             min_index = left
-        if right < len(self.heap) and self.heap[right] > self.heap[min_index]:
+        if right < len(self.heap) and self.heap[right] < self.heap[min_index]:
             min_index = right
-            
         if i != min_index:
-            self.heap[i] ,self.heap[min_index] = self.heap[min_index] , self[i]
+            self.heap[i], self.heap[min_index] = self.heap[min_index], self.heap[i]
             self.heapify_down(min_index)
-            
-            
-    def peekValue(self):
+
+    def peek_min(self):
         if len(self.heap) == 0:
-            print("the priority queue is empty.")
-            logging.info("the priority queue is empty.")
             return None
-        else:
-            return self.heap[0]
-            
-            
-            
-    def printPQ(self):
-        print("printing the priority queue.")
-        logging.info("printing the priority queue.")
-        if len(self.heap) == 0:
-            print("the priority queue is empty.")
-            logging.info("the priority queue is empty.")
-        else:
-            for i in self.heap:
-                print(f"->{i}",end=" ")
-                logging.info(f"->{i}")
+        return self.heap[0]
 
 
-# checking the priority queue
-h1 = PriorityQueue()
+# Example usage:
+pq = PriorityQueue()
+pq.insert(5)
+pq.insert(3)
+pq.insert(8)
+pq.insert(2)
 
-arr = [5,3,8,2]
+print("Minimum element:", pq.peek_min())  # Output: 2
 
-for i in arr:
-    h1.insertPQ(i)   
-    
-h1.printPQ()
-print(f"The most important task of the priority is : {h1.extractMin()}")
+print("Extracted elements in order:")
+while pq.peek_min() is not None:
+    data = pq.extract_min()
+    print(f"->{data}",end=" ")
+    logging.info(f"->{data}")
+    # Output: 2, 3, 5, 8
+
     
     
 
