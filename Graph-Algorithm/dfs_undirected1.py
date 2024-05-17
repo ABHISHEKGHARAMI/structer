24e960d463cbffdb687c2f934d9e0472df89edfc
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-17 21:00:33 +0530

    Added the edges for the graph .

--- Graph-Algorithm/dfs_undirected1.py
+++ Graph-Algorithm/dfs_undirected1.py
@@ -5,3 +5,26 @@ from settings import setup_logging
 import logging
 setup_logging()
 
+class Graph:
+    
+    def __init__(self):
+        self.graph = {}
+        
+        
+    # adding the edge for the undirected graph
+    def addEdge(self,u,v):
+        
+        try:
+            if u not in self.graph:
+                self.graph[u] = []
+            self.graph[u].append(v)
+            
+            
+            # connecting the ede for the undirected graph
+            if v not in self.graph:
+                self.graph[v] = []
+            self.graph[v].append(u)
+        except Exception as e:
+            print(e)
+            logging.info(e)
+            raise e
