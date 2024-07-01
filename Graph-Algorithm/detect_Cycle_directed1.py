522e89270af9662ea094f9274b044d4f9e7e869f
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-01 14:27:03 +0530

    Change lets see work or not .

--- Graph-Algorithm/detect_Cycle_directed1.py
+++ Graph-Algorithm/detect_Cycle_directed1.py
@@ -10,7 +10,7 @@ setup_logging()
 class Graph:
     def __init__(self,V):
         self.graph = {}
-        self.vertices = V
+        self.V = V
         
     # add edge for the Graph
     def addEdge(self,u,v):
@@ -35,71 +35,35 @@ class Graph:
             logging.info(e)
             
             
-    # module for the check cycle for the graph
-    def isCycle(self):
-        visited = [False]*self.vertices
-        rec_stack = [False]*self.vertices
-        
-        for node in range(self.vertices):
-            if not visited[node]:
-                if self.isCycleUtil(node,visited,rec_stack):
-                    return False
-                return True
-            
-            
-    # helper module for the check the each node with the neighbors 
-    def isCycleUtil(self,v,visited,rec_stack):
+    # module for helper function for the checking of the graph
+    def cycleDetectUtil(self,v,visited,rec_stack):
         visited[v] = True
         rec_stack[v] = True
         
         for neighbor in self.graph[v]:
-            if not visited[neighbor]:
-                if self.isCycleUtil(neighbor,visited,rec_stack):
+            if visited[neighbor] == False:
+                if self.cycleDetectUtil(neighbor,visited,rec_stack) == True:
                     return True
-                elif rec_stack[neighbor] :
+                elif rec_stack[neighbor] == True:
                     return True
-            
-        # if this won't the case then the case will be
+                
+        # the node need to pop out for the stack
         rec_stack[v] = False
         return False
     
-    
-    
-v = int(input("enter the vertices for the graph :"))
-
-g1 = Graph(v)
-
-
-while True:
-    print("1: for add edge.\t 2: Check the Cycle.\t 3: Print Graph.")
-    print("0: for exit")
-    choice = int(input("enter the choice :"))
-    if choice == 0:
-        exit(0)
-    elif choice == 1:
-        u ,v = map(int,input("enter the to vertices :").split(","))
-        g1.addEdge(u,v)
-    elif choice == 2:
-        if g1.isCycle():
-            print("Graph contains the cycle.")
-            logging.info("Graph contains the cycle.")
-        else:
-            print("Graph does not contains the cycle.")
-            logging.info("Graph does not contains the cycle.")
-    elif choice == 3:
-        print("Printing the graph.")
-        logging.info("Printing the graph.")
-        g1.printGraph()
-        
-    else:
-        print("Wrong choice mate!!")
-        logging.info("Wrong choice mate!!")
-        
-        print("Here we go again !!")
                 
         
             
 
-
+    # module for the detect cycle for the graph main function
+    def cycleDetect(self):
+        visited = [False] * self.V
+        rec_stack = [False] * self.V
+        for node in range(self.V):
+            if visited[node] == False:
+                if self.cycleDetectUtil(node,visited,rec_stack) == True:
+                    return True
+        return False
+            
         
     
