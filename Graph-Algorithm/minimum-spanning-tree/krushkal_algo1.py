bec8b863521798367b46fb72cfc082866e888e49
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-14 09:32:18 +0530

    .mod

--- Graph-Algorithm/minimum-spanning-tree/krushkal_algo1.py
+++ Graph-Algorithm/minimum-spanning-tree/krushkal_algo1.py
@@ -15,3 +15,33 @@ steps
 
 '''
 
+class DSU:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+        
+    
+    
+    # find the parent
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    
+    
+    # union of the two different parent to avoid cycle
+    def union(self,x,y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        
+        
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.parent[rooty] = rootx
+                
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
