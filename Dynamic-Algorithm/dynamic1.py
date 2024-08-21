f14efeb22917baa7db1fc29221602e871c2a8e3a
abhishek <abhishekgharami1998@gmail.com>
2024-08-21 22:22:21 +0530

    Adding the first dynamic program .

--- Dynamic-Algorithm/dynamic1.py
+++ Dynamic-Algorithm/dynamic1.py
@@ -15,7 +15,31 @@ def fibo_recurr(num):
         return fibo_recurr(num-1) + fibo_recurr(num-2)
     
     
+"""
+
+here we go for the dynamic aproach of the program for the fibonacci series
+in this algorithm we need to store the previous result.
+"""
+
+
+def fibo_Dynamic(num):
+    fibo = [0] * (num+2)
+    fibo[0] = 0
+    fibo[1] = 1
+    
+    for i in range(2,len(fibo)):
+        fibo[i] = fibo[i-1] + fibo[i-2]
+        
+    return fibo[num]
+    
 num = int(input("enter the number to get the fibonnaci :"))
 
 print(f"the fibonacci addition of the {num} is : {fibo_recurr(num)}")
 logging.info("the fibonacci addition of the {num} is : {fibo_recurr(num)}")
+
+
+print(f"After the dynamic approach the result will be : {fibo_Dynamic(num)}")
+logging.info(
+    f"After the dynamic approach the result will be : {fibo_Dynamic(num)}")
+
+
