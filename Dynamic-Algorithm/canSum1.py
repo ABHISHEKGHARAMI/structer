f875601e7c3603c479c293ce83f5473a1b4bb0e7
abhishek <abhishekgharami1998@gmail.com>
2024-09-10 19:20:25 +0530

    Adding the recursion  solution of the program .

---
+++ Dynamic-Algorithm/canSum1.py
@@ -0,0 +1,38 @@
+# setting up the logger file
+import sys
+sys.path.append("/home/abhishek/Documents/DataStructure&Algorithm/structer")
+from settings import setup_logging
+
+import logging
+
+setup_logging()
+
+'''
+problem statement an array and a number is given find the solution to check that 
+array consist the sum of the data in the list.
+'''
+
+# recursion 
+def checkSumRecurr(array,targetSum) :
+    if targetSum == 0:
+        return True
+    if len(array) == 0:
+        return False
+    # case 1 : check the element is greater than the element then ignore it.
+    if array[-1] > targetSum:
+        checkSumRecurr(array[:-1],targetSum)
+    # case 2 : check the two possibility
+    # a : include it the checksum
+    # b : exclude it from the checksum
+    return checkSumRecurr(array[:-1],targetSum-array[-1]) or checkSumRecurr(array[:-1],targetSum)
+
+
+array = map(list,int(input("enter the array element:").split(',')))
+target = int(input("enter the target:"))
+
+if checkSumRecurr(array,target) == True:
+    print(f"{target} can be acheived using the array.")
+    logging.info(f"{target} can be acheived using the array.")
+else:
+    print(f"{target} can not be acheived using the array.")
+    logging.info(f"{target} can not be acheived using the array.")
