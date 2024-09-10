4818cd8802e1b794d4a0b794dba44370da0b47b2
abhishek <abhishekgharami1998@gmail.com>
2024-09-10 20:38:41 +0530

    Adding the dynamic approach to the solution .

--- Dynamic-Algorithm/howSum1.py
+++ Dynamic-Algorithm/howSum1.py
@@ -25,8 +25,29 @@ def recurHowSum(array,target) :
     return None
 
 
+
+# using the dynamic memorization approach
+def howSumDynamic(array,target,memo={}):
+    if target in memo:
+        return memo[target]
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+    for num in array:
+        remainder = target - num
+        remResult = howSumDynamic(array,remainder,memo)
+        if remResult != None:
+            memo[target] = [remainder,num]
+            
+    return None
+
+
 arr = [4,5,2,1]
 target = 9
 
 print(f"the combination is : {recurHowSum(arr,target)}")
 logging.info(f"the combination is : {recurHowSum(arr,target)}")
+
+print(f"the combination using the dynamic approach  is : {howSumDynamic(arr,target)}")
+logging.info(f"the combination using the dynamic approach is : {howSumDynamic(arr,target)}")
