a43209b580eaaee361b90e78d3ac58ec2699eb6d
abhishek <abhishekgharami1998@gmail.com>
2024-09-24 21:14:15 +0530

    # mod

--- Dynamic-Algorithm/gridDynamictabular1.py
+++ Dynamic-Algorithm/gridDynamictabular1.py
@@ -14,15 +14,21 @@ you can go from start to end using the dynamic tabular approach.
 '''
 
 def grid_dynamics(m,n):
-    grid = [[0]*n for _ in range(m)]
-    grid[0][0] = 0
+    grid = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    
+    #base case
     grid[1][1] = 1
     
-    for i in range(2,m):
-        for j in range(2,n):
-            grid[i][j] = grid[i-1][j] + grid[i][j-1]
+    for i in range(m+1):
+        for j in range(n+1):
+            current = grid[i][j]
             
-    return grid[m-1][n-1]
+            if i+1<=m:
+                current += grid[i+1][j]
+            if j + 1 <= n :
+                current += grid[i][j+1]
+                
+    return grid[m][n]
 
 print(f"the step require for the grid is : {grid_dynamics(3,4)}")
 logging.info(f"the step require for the grid is : {grid_dynamics(3,4)}")
