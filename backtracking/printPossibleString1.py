839887a3ead824b41a979de6488959f67962b66a
abhishek <abhishekgharami1998@gmail.com>
2024-10-11 12:48:10 +0530

    Adding the backtracking sol for the print possible string .

---
+++ backtracking/printPossibleString1.py
@@ -0,0 +1,19 @@
+'''
+Given a string you need to print all possible strings that can be made by placing spaces 
+(zero or one) in between them. 
+
+Input:  str[] = "ABC"
+Output: ABC
+        AB C
+        A BC
+        A B C
+'''
+
+# setting up the logger file
+import sys
+sys.path.append("/home/abhishek/Documents/DataStructure&Algorithm/structer")
+from settings import setup_logging
+
+import logging
+
+setup_logging()
